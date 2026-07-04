5c8fb8086bc1b5b86197325b15b7e27912e56726
JaimeGV15 <jaime.007.garciav@gmail.com>
2026-07-04 22:15:04 +0000

    added suggesiton to converter.cpp

--- src/converter.cpp
+++ src/converter.cpp
@@ -3,59 +3,71 @@
 
 using namespace std;
 
-double inchesToCentimeters(double inches) {
+double inchesToCentimeters(double inches) 
+{
 
     return inches * CENTIMETERS_PER_INCH;
 }
 
-double centimetersToInches(double centimeters) {
+double centimetersToInches(double centimeters) 
+{
     // TODO: return centimeters converted to inches.
     return centimeters / CENTIMETERS_PER_INCH;
 }
 
-double poundsToKilograms(double pounds) {
+double poundsToKilograms(double pounds) 
+{
     // TODO: return pounds converted to kilograms.
     return pounds / POUNDS_PER_KILOGRAM;
 }
 
-double kilogramsToPounds(double kilograms) {
+double kilogramsToPounds(double kilograms) 
+{
     // TODO: return kilograms converted to pounds.
     return kilograms * POUNDS_PER_KILOGRAM;
 }
 
-double fahrenheitToCelsius(double fahrenheit) {
+double fahrenheitToCelsius(double fahrenheit) 
+{
     // TODO: return Fahrenheit converted to Celsius.
-    return ((fahrenheit - 32) * 5 / 9);
+    return ((fahrenheit - 32) * (5.0 / 9.0)); // Jaime: added a parenthesis to close the 5/9, so its easier to see in the part of the formula, it's a matter of personal taste, more than anything.
 }
 
-double celsiusToFahrenheit(double celsius) {
+double celsiusToFahrenheit(double celsius) 
+{
     // TODO: return Celsius converted to Fahrenheit.
-    return ((celsius * 9 / 5) + 32);
+    return ((celsius * (9.0 / 5.0)) + 32); // Jaime: I added a parenthesis here also, the same as in fahrenheit to celsius, personal taste
 }
 
-bool isValidMenuChoice(int choice) {
+bool isValidMenuChoice(int choice) 
+{
     // TODO: return true when choice is between EXIT_CHOICE and CELSIUS_TO_FAHRENHEIT.
     return choice >= EXIT_CHOICE && choice <= CELSIUS_TO_FAHRENHEIT;
 }
 
-bool requiresNonNegativeValue(int choice) {
+bool requiresNonNegativeValue(int choice) 
+{
     // Length and weight conversions cannot use negative values.
     // Temperature conversions may use negative values.
     // TODO: return true for choices 1 through 4.
     return choice >= INCHES_TO_CENTIMETERS && choice <= KILOGRAMS_TO_POUNDS;
 }
 
-bool isValidValueForChoice(int choice, double value) {
+bool isValidValueForChoice(int choice, double value) 
+{
   
-    if(!isValidMenuChoice(choice)) {
+    if(!isValidMenuChoice(choice)) 
+    {
     return false;
 }
-if(requiresNonNegativeValue(choice) && value < 0){
+if(requiresNonNegativeValue(choice) && value < 0)
+{
     return false;
 }
     return true;
 }
-void printMenu() {
+void printMenu() 
+{
     cout << endl;
     cout << "Unit Converter" << endl;
     cout << endl;
@@ -69,4 +81,4 @@ void printMenu() {
     cout << endl;
     cout << "Enter choice: ";
     
-}
+} //Jaime: I added an extra space to give the intial square bracket its own line, so its easier to see where it begins and where it ends.
